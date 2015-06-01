9aa751141b6120a3c58cba5e9f927fcf15904e70
Ramphd <wadqse@126.com>
2015-06-01 16:46:38 +0800

    #add move steps and commet some codes

--- src/test/algri/sort/SortMethods.java
+++ src/test/algri/sort/SortMethods.java
@@ -9,23 +9,27 @@ public class SortMethods {
 	public <T> List<T> bubbleSort(List<T> array) {
 		@SuppressWarnings("unchecked")
 		T[] a = (T[]) array.toArray();
-		if (array.size() > 1) {
-			System.out.println("origin:" + array);
-			int initSize = a.length;
+		int initSize = a.length;
+		if (initSize > 1) {
+			int moves = 0;
+			System.out.print("bubble-origin:" + array);		
 			boolean done = false;// judge ok or not
 			for (int i = 1; i < initSize; i++) {
 				done = true;
+				++moves;
 				for (int j = 0; j < initSize - i; j++) {
 					if (compare(a[j], a[j + 1]) > 0) {
 						swapInt(a, j, j + 1);
 						swap(array, j, j + 1);
-						System.out.println("process:" + array);
+//						System.out.println("process:" + array);
 						done = false;
 					}
 				}
-				if (done)
+			
+				if (done)					
 					break;
 			}
+			System.out.println("moves"+ (moves-1));
 		}
 		return array;
 	}
@@ -33,7 +37,7 @@ public class SortMethods {
 	private <T> int compare(T x, T y) {
 		int a1 = (java.lang.Integer) x;
 		int a2 = (java.lang.Integer) y;
-		System.out.println(a1 - a2);
+//		System.out.println(a1 - a2);
 		return (a1 - a2);
 	}
 
